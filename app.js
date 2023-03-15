8eda3de988ca3b4cbbe1e81356936d052d03dbcf
Julien <juliendorra@juliendorra.com>
2023-03-15 23:02:11 +0100

    save data in an array; can ref another groupe result; handle request limits per groups
    Bug: data text is not reverted in case of reference or error

--- app.js
+++ app.js
@@ -1,34 +1,102 @@
 import { callGPT } from './gpt.js';
 
-function addEventListenersToGroup(group) {
+const groups = [];
+
+function updateDataTextarea(textarea, groupName) {
+    const group = groups.find(group => group.name === groupName);
+    if (!group) {
+        textarea.classList.add('error');
+        textarea.value = 'Error: Group not found.';
+        return;
+    }
+
+    textarea.classList.remove('error');
+    textarea.value = group.result ? group.result : `#${groupName}`;
+}
+
+function addEventListenersToGroup(group, index) {
     let timer = null;
+    let lastRequestTime = 0;
+    let latestRequestId = 0;
+
+    function handleInputChange() {
+        console.log('handleInputChange called');
+        clearTimeout(timer);
+        const currentTime = Date.now();
+
+        if (currentTime - lastRequestTime < 5000) {
+            console.log('Waiting for 5 seconds');
+            timer = setTimeout(handleInputChange, 5000 - (currentTime - lastRequestTime));
+            return;
+        }
+
+        lastRequestTime = currentTime;
+        const dataInput = group.querySelector('.data-text');
+        const transformInput = group.querySelector('.transform-text');
+
+        if (dataInput.value && transformInput.value) {
+            latestRequestId++;
+            const requestId = latestRequestId;
+            console.log(`Sending request ${requestId} with ${dataInput.value} and ${transformInput.value}`);
+            callGPT(dataInput.value, transformInput.value).then((result) => {
+                console.log(`Received result for request ${requestId}`);
+                if (requestId !== latestRequestId) return;
+
+                groups[index].result = result;
+
+                let resultParagraph = group.querySelector('.result');
+
+                if (!resultParagraph) {
+                    resultParagraph = document.createElement('p');
+                    resultParagraph.className = 'result';
+                    group.appendChild(resultParagraph);
+                }
+
+                resultParagraph.textContent = result;
+
+                // Update all data textareas with the new result
+                document.querySelectorAll('.data-text').forEach(dataTextarea => {
+                    const groupNameMatch = dataTextarea.value.match(/#(.+)/);
+                    if (groupNameMatch && groupNameMatch[1] === groups[index].name) {
+                        updateDataTextarea(dataTextarea, groupNameMatch[1]);
+                    }
+                });
+            });
+        }
+    }
 
     group.querySelectorAll('.data-text, .transform-text').forEach(input => {
-        input.addEventListener('input', () => {
-            clearTimeout(timer);
+        input.addEventListener('change', handleInputChange);
+    });
 
-            const dataInput = group.querySelector('.data-text');
-            const transformInput = group.querySelector('.transform-text');
+    group.querySelector('.group-name').addEventListener('input', (event) => {
+        groups[index].name = event.target.value;
+        console.log(`Group ${index} name now:${groups[index].name})`)
+    });
 
-            if (dataInput.value && transformInput.value) {
-                timer = setTimeout(async () => {
-                    const result = await callGPT(dataInput.value, transformInput.value);
-                    let resultParagraph = group.querySelector('.result');
+    const dataTextarea = group.querySelector('.data-text');
+    dataTextarea.addEventListener('input', () => {
+        const groupNameMatch = dataTextarea.value.match(/#(.+)/);
 
-                    if (!resultParagraph) {
-                        resultParagraph = document.createElement('p');
-                        resultParagraph.className = 'result';
-                        group.appendChild(resultParagraph);
-                    }
+        if (groupNameMatch) {
+            updateDataTextarea(dataTextarea, groupNameMatch[1]);
+        }
+    });
 
-                    resultParagraph.textContent = result;
-                }, 3000);
-            }
-        });
+    dataTextarea.addEventListener('focus', () => {
+        dataTextarea.classList.remove('error');
+        const originalValue = groups[index].data;
+        if (originalValue) {
+            dataTextarea.value = originalValue;
+        }
+    });
+
+    dataTextarea.addEventListener('blur', () => {
+        groups[index].data = dataTextarea.value;
     });
 }
 
-document.querySelector('.add-group-btn').addEventListener('click', function () {
+function addGroup() {
     const group = document.createElement('div');
     group.className = 'group';
     group.innerHTML = `
@@ -40,7 +108,12 @@ document.querySelector('.add-group-btn').addEventListener('click', function () {
     const btn = document.querySelector('.add-group-btn');
     btn.parentNode.insertBefore(group, btn);
 
-    addEventListenersToGroup(group);
-});
+    const index = groups.length;
+    groups.push({ name: '', data: '', transform: '', result: null });
 
-addEventListenersToGroup(document.querySelector('.group'));
+    addEventListenersToGroup(group, index);
+}
+
+document.querySelector('.add-group-btn').addEventListener('click', addGroup);
+
+addGroup();
