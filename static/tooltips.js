7ef81dadd89f3012dc7ab2a24af9599a8caba141
Julien <juliendorra@juliendorra.com>
2024-01-31 23:56:19 +0100

    fix tooltip texts, custom styling

--- static/tooltips.js
+++ static/tooltips.js
@@ -1,5 +1,5 @@
 
-import tippy from "https://cdn.jsdelivr.net/npm/tippy.js@6/+esm";
+import tippy, { roundArrow } from "https://cdn.jsdelivr.net/npm/tippy.js@6/+esm";
 // import 'https://cdn.jsdelivr.net/npm/tippy.js@6/dist/tippy.min.css';
 
 export { setTooltips, setAccessibleDescriptions };
@@ -8,24 +8,24 @@ const TOOLTIPS =
     [
         {
             selector: ".entry-btn",
-            text: "Entry: make the data field an ephemeral input. It'll be blank when the app is reloaded, ready for an user to fill it in. The transform is made read-only and saved for later use."
+            text: "Entry: makes the data field an ephemeral input. It'll be blank when the app is reloaded, ready for an user to fill it in. The transform is made read-only and saved for later use."
         },
 
         {
             selector: ".lock-btn",
-            text: "Lock: make the data and transform fields read-only. Both are saved for later use"
+            text: "Lock: makes the data and transform fields read-only. Both are saved for later use"
         },
         {
             selector: ".list-mode-btn",
-            text: "List mode: turn a list-like result into a list dropdown, so you can choose one of the answer"
+            text: "List mode: turns a list-like text result into a list dropdown, so you can choose one of the answer"
         },
         {
             selector: ".refresh-btn",
-            text: "Refresh: reaque to generate a new result"
+            text: "Refresh: requests to generate a new result"
         },
         {
             selector: ".controlnet-btn",
-            text: "Controlnet mode: the structure of image referenced in the data field will be used to constraint the image. When off, the image is used as a starting point (image-to-image)"
+            text: "Controlnet mode: the structure of the image referenced in the data field will be used to constrain the image. When off, the image is used as a starting point (image-to-image). Note that only the first image referenced is used in both case."
         }
     ]
 
@@ -54,7 +54,10 @@ function setTooltips(tooltipsEnabled) {
             tooltipInstances.push(
                 ...tippy(
                     tooltip.selector,
-                    { content: tooltip.text, }
+                    {
+                        content: tooltip.text,
+                        theme: 'esquisse',
+                    }
                 )
             );
         }
