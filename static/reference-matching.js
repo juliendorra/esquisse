3021c3cbc7d3ed80a16e1d595b724ffa3ba18ce5
Julien Dorra <juliendorra@juliendorra.com>
2024-07-15 14:00:18 +0200

    Create itemized list of text and references from dataText
    the getReferencedResultsAndCombinedDataWithResults function now returns a proper itemized list of every text part and dereferenced result from the dataText input by the user.
    Each item has metadata that will allow us to properly build the referenced-result preview so the user gets proper feedback on creating their prompts and inputs
    
    also delete unused blobTo8CharsHash

--- static/reference-matching.js
+++ static/reference-matching.js
@@ -1,5 +1,4 @@
 import { GROUP_TYPE, getGroupFromName } from "./group-utils.js";
-
 import { referencesGraph, updateReferenceGraph } from "./reference-graph.js";
 import { displayAlert } from "./ui-utils.js";
 
@@ -55,15 +54,9 @@ function replaceThisGroupReferenceWithResult(name, data, groups) {
 
         // Replace each match of the exact pattern 'this group name surrounded by brackets'
         replacedData = replacedData.replace(combinedPattern, () => referencedGroup.result);
-
-    }
-
-    if (isImageResult) {
-
-        // Replace image result reference with IMG tag
-
+    } else {
         replacedData = replacedData.replace(combinedPattern, (match) => {
-            console.log("Replace image references with IMG tags ", match);
+            console.log("[REFERENCE MATCHING] Replace image references with IMG tags ", name, match);
 
             const blobUrl = referencedGroup.resultBlobURI;
 
@@ -73,6 +66,7 @@ function replaceThisGroupReferenceWithResult(name, data, groups) {
 
     return replacedData;
 }
+
 
 async function getReferencedResultsAndCombinedDataWithResults(dataText, currentGroupName, groups) {
 
@@ -85,28 +79,56 @@ async function getReferencedResultsAndCombinedDataWithResults(dataText, currentG
     let availableReferencedResults = []; //[{ name, result, type, resultHash },..} 
 
     let combinedReferencedResults = dataText;
+    let itemizedDataText = []; //[{ name, isReference, isValid, isReady, groupType, contentType, resultToInsert},..} 
+
+    console.log(`[REFERENCE MATCHING] matching refs in group ${currentGroupName}`);
 
     if (hasReferences) {
 
         const currentGroup = getGroupFromName(currentGroupName, groups);
 
-        for (const name of namesOfAllGroupsReferencedByThisGroup) {
+        let lastIndex = 0;
+
+        for (const match of dataText.matchAll(REFERENCE_MATCHING_REGEX)) {
+
+            const name = match[1] || match[2];
+
+            console.log(`[REFERENCE MATCHING] in group ${currentGroupName} reference name is ${name}`);
+
+
+            // we iterativeley replace each match with the result in combinedReferencedResult
+            combinedReferencedResults = replaceThisGroupReferenceWithResult(name, combinedReferencedResults, groups);
+
+            // is there some text content preceding this reference?
+
+            const precedingText = dataText.substring(lastIndex, match.index);
+            if (precedingText) {
+                itemizedDataText.push({ name: `before ${name}`, isReference: false, isValid: true, isReady: true, groupType: null, contentType: "TEXT", resultToInsert: precedingText });
+            }
+
             const referencedGroup = getGroupFromName(name, groups);
+
+            // The reference is not corresponding to any existing group
+            // we just the name as content
+            // we'll use the values to render an appropriate error in the preview 
 
             if (!referencedGroup) {
                 console.log(`[REFERENCE MATCHING] Trying to show reference but no group "${name}" found`);
 
-                displayAlert(
-                    {
-                        issue: `No group "${name}" found`,
-                        action: `Fix the reference in "${currentGroupName}"`,
-                        variant: "warning",
-                        icon: "question-circle",
-                        duration: 3000
-                    }
-                );
+                displayAlert({
+                    issue: `No group "${name}" found`,
+                    action: `Fix the reference in "${currentGroupName}"`,
+                    variant: "warning",
+                    icon: "question-circle",
+                    duration: 3000
+                });
 
                 invalidReferencedResults.push(name);
+
+                itemizedDataText.push({ name, isReference: true, isValid: false, isReady: false, groupType: null, contentType: null, resultToInsert: name });
+
+                lastIndex = match.index + match[0].length;
+
                 continue;
             }
 
@@ -114,23 +136,55 @@ async function getReferencedResultsAndCombinedDataWithResults(dataText, currentG
             // We update the inverse reference graph
             updateReferenceGraph(groups)
 
-            const hasDirectCircularReference = referencesGraph.IS_USED_BY_GRAPH.hasEdge(referencedGroup.id, currentGroup.id) && referencesGraph.IS_USED_BY_GRAPH.hasEdge(currentGroup.id, referencedGroup.id);
+            // We identify the overall type of the content, used to render them appropriately
+            let contentType;
+            switch (referencedGroup.type) {
+
+                case GROUP_TYPE.BREAK:
+                    contentType = null;
+                    break;
+
+                case GROUP_TYPE.TEXT:
+                    contentType = 'HTML';
+                    break;
+
+                case GROUP_TYPE.STATIC:
+                    contentType = 'HTML';
+                    break;
+
+                case GROUP_TYPE.IMPORTED_IMAGE:
+                    contentType = 'IMAGE'
+                    break;
+
+                case GROUP_TYPE.IMAGE:
+                    contentType = 'IMAGE'
+                    break;
+
+                default:
+                    contentType = null;
+            }
+
+            const hasDirectCircularReference = referencesGraph.IS_USED_BY_GRAPH.hasEdge(referencedGroup.id, currentGroup.id) &&
+                referencesGraph.IS_USED_BY_GRAPH.hasEdge(currentGroup.id, referencedGroup.id);
             const isSelfReference = referencedGroup.id === currentGroup.id;
 
             if (hasDirectCircularReference || isSelfReference) {
                 console.log(`[REFERENCE MATCHING] Direct circular reference between ${currentGroupName} and ${name}`);
 
-                displayAlert(
-                    {
-                        issue: `Circular reference between "${currentGroupName}" and "${name}"`,
-                        action: "Remove circular references",
-                        variant: "warning",
-                        icon: "arrow-repeat",
-                        duration: 3000
-                    }
-                );
+                displayAlert({
+                    issue: `Circular reference between "${currentGroupName}" and "${name}"`,
+                    action: "Remove circular references",
+                    variant: "warning",
+                    icon: "arrow-repeat",
+                    duration: 3000
+                });
 
                 invalidReferencedResults.push(name);
+
+                itemizedDataText.push({ name, isReference: true, isValid: false, isReady: false, groupType: referencedGroup.type, contentType: contentType, resultToInsert: name });
+
+                lastIndex = match.index + match[0].length;
+
                 continue;
             }
 
@@ -138,40 +192,81 @@ async function getReferencedResultsAndCombinedDataWithResults(dataText, currentG
                 console.log(`[REFERENCE MATCHING] The result for "${name}" is not existing, and can't be used by group "${currentGroupName}" when trying to get referenced results`);
 
                 notreadyReferencedResults.push(name);
+
+                itemizedDataText.push({ name, isReference: true, isValid: true, isReady: false, groupType: referencedGroup.type, contentType: contentType, Insert: name });
+
+                lastIndex = match.index + match[0].length;
+
                 continue;
             }
 
-            combinedReferencedResults = await replaceThisGroupReferenceWithResult(name, combinedReferencedResults, groups);
+            const resultToInsert = await getResultToInsert(name, groups);
 
-            availableReferencedResults.push({ name, result: referencedGroup.result, type: referencedGroup.type, resultHash: referencedGroup.resultHash });
+            itemizedDataText.push({ name, isReference: true, isValid: true, isReady: true, groupType: referencedGroup.type, contentType: contentType, resultToInsert });
+
+            lastIndex = match.index + match[0].length;
+
+            const result = getResult(name, groups);
+
+            availableReferencedResults.push({ name, result, type: referencedGroup.type, resultHash: referencedGroup.resultHash });
+        }
+
+        // After looping over all references, check if there's text left at the end
+
+        const remainingText = dataText.substring(lastIndex);
+
+        if (remainingText) {
+            itemizedDataText.push({ name: "closing text", isReference: false, isValid: true, isReady: true, groupType: null, contentType: "TEXT", resultToInsert: remainingText });
+        }
+
+    } else {
+        if (dataText) {
+            itemizedDataText.push({ name: "full text", isReference: false, isValid: true, isReady: true, groupType: null, contentType: "TEXT", resultToInsert: dataText });
         }
     }
 
+    console.log("[REFERENCE MATCHING] itemized", itemizedDataText, " combined ", combinedReferencedResults);
 
     return {
         hasReferences,
         invalidReferencedResults,
         notreadyReferencedResults,
         availableReferencedResults,
+        itemizedDataText,
         combinedReferencedResults
     };
 }
 
-// utils
+// Utils
 
-async function blobTo8charsHash(blob) {
+function getResultToInsert(name, groups) {
 
-    const arrayBuffer = await new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsArrayBuffer(blob);
-    });
+    const referencedGroup = getGroupFromName(name, groups);
 
-    const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+    if (!referencedGroup || referencedGroup.result === undefined) {
 
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return "";
+    }
 
-    return hashHex.substring(0, 8);
+    const isImageResult =
+        (referencedGroup.type === GROUP_TYPE.IMAGE)
+        || (referencedGroup.type === GROUP_TYPE.IMPORTED_IMAGE);
+
+    if (!isImageResult) {
+        return referencedGroup.result;
+    } else {
+        return referencedGroup.resultBlobURI;
+    }
+}
+
+function getResult(name, groups) {
+
+    const referencedGroup = getGroupFromName(name, groups);
+
+    if (!referencedGroup || referencedGroup.result === undefined) {
+
+        return "";
+    }
+
+    return referencedGroup.result;
 }
